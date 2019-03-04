b76d7c7b546fa9a7ab14f70f2fdf03104ead9b5f
Samuel Tallent <samuel.tallent@silvercar.com>
2019-03-04 14:15:34 -0600

    Update version for release

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(fname):
 
 setup(
     name='ecs-cluster',
-    version='1.1.1.test-p3.1',
+    version='1.1.2',
     author='Silvercar',
     author_email="info@silvercar.com",
     url='https://github.com/silvercar/ecs-cluster',
